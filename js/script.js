d044e155836525c82ff1e0d08b7349474007f79f
Irvel <irvelnduva@hotmail.com>
2015-05-02 18:47:29 -0500

    Add function to match page colors with color picked in the slider

--- js/script.js
+++ js/script.js
@@ -1,24 +1,5 @@
 
 function cambiarColor(){
-  var x = document.getElementById("sliderPrincipal").value;
-  if(x < 50){
-    document.getElementById("sliderPrincipal").style.backgroundColor = "#FFFFFF";
-  }
-}
-
-
-window.onload=function()
-{
- var classname = document.getElementsByClassName("PIDControlSlider");
-
-    var myFunction = function() {
-        var attribute = this.getAttribute("sliderPrincipal");
-//Your code goes here
-document.getElementById("sliderPrincipal").style.backgroundColor = "#FFFFFF";
-        socket.emit('SCMD', this.getAttribute("sliderPrincipal")+' '+ this.value);
-    };
-
-    for(var i=0;i<classname.length;i++){
-        classname[i].addEventListener('change', myFunction, false);
-    }
+  document.getElementById("cuerpo").style.backgroundColor = '#'+this.color;
+  document.getElementById("barra").style.backgroundColor = '#'+this.color;
 }
